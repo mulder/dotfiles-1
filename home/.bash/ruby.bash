7aab6a7528b321f65f20765e4a6f1511cf9e439b
Nicholas Mulder <nicholas.mulder@gmail.com>
2015-05-04 10:09:41 -0400

    use rbenv

--- home/.bash/ruby.bash
+++ home/.bash/ruby.bash
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-if [[ -r /usr/local/share/chruby/chruby.sh && -z $SHOPIFY_DEV_VAGRANT ]]; then
-  source /usr/local/share/chruby/chruby.sh
-  RUBIES=(~/.rubies/*)
+# if [[ -r /usr/local/share/chruby/chruby.sh && -z $SHOPIFY_DEV_VAGRANT ]]; then
+#   source /usr/local/share/chruby/chruby.sh
+#   RUBIES=(~/.rubies/*)
 
-  # switch to latest stable
-  chruby $(chruby | grep -v trunk | tail -n1 | awk '{print $1}')
+#   # switch to latest stable
+#   chruby $(chruby | grep -v trunk | tail -n1 | awk '{print $1}')
 
-  source /usr/local/share/chruby/auto.sh
+#   source /usr/local/share/chruby/auto.sh
+# fi
+
+
+if [[ -r $HOME/.rbenv && -z $SHOPIFY_DEV_VAGRANT ]]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
 fi
